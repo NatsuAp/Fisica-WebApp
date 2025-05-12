99b3de6d6d2f8cd6e06a784bb09b3805edaf4b3d
NatsuAp <166424229+NatsuAp@users.noreply.github.com>
2025-05-12 12:19:42 -0500

    Update connection.py

--- arduino/connection.py
+++ arduino/connection.py
@@ -7,7 +7,7 @@ def checkConnection():
      #El primer parametro es el puerto
      #baudrate es la velocidad de transmision de datos en baudios pr segundo TODO:Verificar en el arudino el "Serial.begin(9600);", en este caso se usara 9600 por defecto
      #timeout es la cantidad de tiempo que esperara para leer datos antes de rendirse
-        arduino = serial.Serial('/dev/tty.usbmodem11101', baudrate=9600, timeout=1)
+        arduino = serial.Serial('COM3', baudrate=9600, timeout=1)
         
     except:
          print("Error de conexion con arduino")
